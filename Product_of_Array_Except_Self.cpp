c901692383aade190cebbae7fbd735811db26dd0
sourav-barman <78752547+sourav-barman@users.noreply.github.com>
2022-10-11 20:55:10 +0530

    Create Product_of_Array_Except_Self.cpp

---
+++ Product_of_Array_Except_Self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> answer(nums.size());
+        
+        int prefix = 1;
+        for(int i=0; i<nums.size(); i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+        
+        int postfix = 1;
+        for(int i=nums.size()-1; i>=0; i--) {
+            answer[i] *= postfix;
+            postfix *= nums[i];
+        }
+        
+        return answer;
+    }
+};
